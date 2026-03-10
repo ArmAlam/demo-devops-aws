197f80d5654e464ca5d59f75c4865125ee7cbabb
arman <arman.u.alam@gmail.com>
2026-03-11 00:50:30 +0600

    add prefix /api

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
